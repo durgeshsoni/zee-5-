6a6a1af8676804a07fa497087b7d16a6de9f9608
prayaschhetri1 <prayashchhetri221@gmail.com>
2022-02-24 21:22:23 +0530

    music

---
+++ bengaliMovies.js
@@ -0,0 +1,33 @@
+var top10BengaliMovies=[
+    {
+        img_Url: "https://akamaividz2.zee5.com/image/upload/w_504,h_756,c_scale,f_webp,q_auto:eco,dpr_2.0/resources/0-0-1z588913/portrait/1920x77062e38594ff8e4f5092d8f1ddd6e233cd.jpg"
+    },
+    {
+       img_url:"https://akamaividz2.zee5.com/image/upload/w_504,h_756,c_scale,f_webp,q_auto:eco,dpr_2.0/resources/0-6-4z571841/portrait/1920x770cd4cf3bcdae84d1eb76c48beca87b4e1.jpg"
+    },
+    {
+        img_url:"https://akamaividz2.zee5.com/image/upload/w_504,h_756,c_scale,f_webp,q_auto:eco,dpr_2.0/resources/0-0-1z571422/portrait/1920x770845d575b57cb43599009f12008dba3a2.jpg"
+     },
+     {
+        img_url:"https://akamaividz2.zee5.com/image/upload/w_504,h_756,c_scale,f_webp,q_auto:eco,dpr_2.0/resources/0-0-1z528078/portrait/1920x770096d9c5039d440cda052ffa4e7b325be.jpg"
+     },
+     {
+        img_url:"https://akamaividz2.zee5.com/image/upload/w_504,h_756,c_scale,f_webp,q_auto:eco,dpr_2.0/resources/0-0-1z531613/portrait/1920x7703f10f5f707be4f74b6d95390ccc0becb.jpg"
+     },
+     {
+        img_url:"https://akamaividz2.zee5.com/image/upload/w_504,h_756,c_scale,f_webp,q_auto:eco,dpr_2.0/resources/0-0-1z515620/portrait/1920x7705e9c7ba0ef154ff1b68037fec26dca73.jpg"
+     },
+     {
+        img_url:"https://akamaividz2.zee5.com/image/upload/w_504,h_756,c_scale,f_webp,q_auto:eco,dpr_2.0/resources/0-6-2177/portrait/1920x770a3c7ba0396c64e6abaa175b6af59a9e1c46673b3db9b437296adf6bdddd2fea4.jpg"
+     },
+     {
+        img_url:"https://akamaividz2.zee5.com/image/upload/w_504,h_756,c_scale,f_webp,q_auto:eco,dpr_2.0/resources/0-0-105190/portrait/00105190incove38709070838784eb1b85164a44f4bec29097560ec648.jpg"
+     },
+     {
+        img_url:"https://akamaividz2.zee5.com/image/upload/w_504,h_756,c_scale,f_webp,q_auto:eco,dpr_2.0/resources/0-0-1z590927/portrait/1920x770b742ef890d6c47978b4cb3b9e7d1c75d.jpg"
+     },
+     {
+        img_url:"https://akamaividz2.zee5.com/image/upload/w_504,h_756,c_scale,f_webp,q_auto:eco,dpr_2.0/resources/0-0-1z571920/portrait/1920x770ec7de642af8640bea8feb0b73278432c.jpg"
+     }
+    
+]
