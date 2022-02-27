378496c53941cd849669338039e7a4e4a30324a1
Durgesh Soni <81063456+durgeshsoni@users.noreply.github.com>
2022-02-27 11:22:45 +0530

    Update more2.js

--- js/more2.js
+++ js/more2.js
@@ -41,12 +41,12 @@ displayWebseries(moviesArr);
 
 
 
-// function sortfunc(){
-//     var sort = document.querySelector('#sort').value;
+function sortfunc(){
+    var sort = document.querySelector('#sort').value;
         
-//     filterData = webseriesArr.filter(function (elem) {
-//         return elem.cat == sort;
-//     })
-//     displayWebseries(filterData)
+    filterData = webseriesArr.filter(function (elem) {
+        return elem.cat == sort;
+    })
+    displayWebseries(filterData)
 //     console.log(filterData);
-// }
+}
